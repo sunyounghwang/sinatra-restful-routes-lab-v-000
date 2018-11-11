45d2fec6de15bf582421dfca09138e6d445ccc23
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-11 20:39:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < Sinatra::Base
 
   post "/recipes" do
     Post.create(name: params[:name], cook_time: params[:cook_time], ingredients: params[:ingredients])
+    erb :index
   end
 
   get "/recipes/:id" do
