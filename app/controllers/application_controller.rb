64947c35bf03fe94292dff36b4d84c799f853e58
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-11 20:51:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
 
   patch "/recipes/:id/" do
     current_recipe.update(name: params[:name], cook_time: params[:cook_time], ingredients: params[:ingredients])
-    redirect to "/recipes/#{last_created_recipe.id}"
+    redirect to "/recipes/#{current_recipe.id}"
   end
 
   helpers do
